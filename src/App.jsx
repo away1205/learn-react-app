70bb13eb91003a7537ffc705ec65d92bdf9d4485
Away <agwamayatullah@gmail.com>
2023-07-29 08:01:54 +0700

    Create Learn React Simple App

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+function App() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps /> */}
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((curStep) => curStep - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((curStep) => curStep + 1);
+  }
+
+  return (
+    <>
+      <div>
+        <button className='close' onClick={() => setIsOpen((is) => !is)}>
+          &times;
+        </button>
+        {isOpen && (
+          <div className='steps'>
+            <div className='numbers'>
+              <div className={step >= 1 ? 'active' : ''}>1</div>
+              <div className={step >= 2 ? 'active' : ''}>2</div>
+              <div className={step >= 3 ? 'active' : ''}>3</div>
+            </div>
+
+            <div className='message'>
+              Step {step}: {messages[step - 1]}
+            </div>
+
+            <div className='buttons'>
+              <button
+                style={{ backgroundColor: '#7950f2', color: 'white' }}
+                onClick={handlePrevious}
+              >
+                previous
+              </button>
+              <button
+                style={{ backgroundColor: '#7950f2', color: 'white' }}
+                onClick={handleNext}
+              >
+                next
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
